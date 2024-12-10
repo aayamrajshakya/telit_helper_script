edaaca65de326ab96862766ece953e4a77cb1428
Aayam Raj Shakya <aayamrajshakya@pm.me>
2024-12-10 12:28:12 -0600

    Improved bash scripts

--- start_modem.sh
+++ start_modem.sh
@@ -1,29 +1,25 @@
 #!/bin/bash
 
-devices=$(ls /dev/cdc-wdm* 2>/dev/null)
-
-if [ -z "$devices" ]; then
+device=$(ls /dev/cdc-wdm* 2>/dev/null | head -n 1)
+if [ -z "$device" ]; then
     echo "No device found"
     exit 1
 fi
 
-device=$(echo "$devices" | head -n 1)
-wwan_interface=$(sudo qmicli -d "$device" --get-wwan-iface)
-
+wwan_interface=$(sudo qmicli -d "$device" --get-wwan-iface 2>/dev/null)
 if [ -z "$wwan_interface" ]; then
     echo "Failed to get WWAN interface"
     exit 1
 fi
 
-echo "Device : $device and WWAN interface: $wwan_interface"
+echo "Device: $device, WWAN interface: $wwan_interface"
 
-check_connection_status() {
-    status=$(sudo qmicli -p -d "$device" --wds-get-packet-service-status)
-    if [[ $status == *"Connection status: 'connected'"* ]]; then
-        return 0
-    else
-        return 1
-    fi
+# New addition
+# Set system selection preference to 5GNR right after detecting the module
+sudo qmicli -d "$device" --nas-set-system-selection-preference=5gnr
+
+check_connection() {
+    sudo qmicli -p -d "$device" --wds-get-packet-service-status | grep -q "Connection status: 'connected'"
 }
 
 setup_connection() {
@@ -32,27 +28,23 @@ setup_connection() {
     sudo qmicli -d "$device" --dms-set-operating-mode='online'
     sleep 1
     sudo ip link set "$wwan_interface" down
-    sleep 1
-    echo 'Y' | sudo tee /sys/class/net/"$wwan_interface"/qmi/raw_ip
-    sleep 2
+    echo 'Y' | sudo tee /sys/class/net/"$wwan_interface"/qmi/raw_ip > /dev/null
     sudo ip link set "$wwan_interface" up
-    sleep 1
     sudo qmicli -p -d "$device" --device-open-net='net-raw-ip|net-no-qos-header' --wds-start-network="apn='internet',ip-type=4" --client-no-release-cid
-    sleep 2
 }
 
 while true; do
     setup_connection
     
-    if check_connection_status; then
-        echo "Device is connected. Continuing with current settings."
+    if check_connection; then
+        echo "Device is connected."
         sudo qmicli -p -d "$device" --wds-get-current-settings
         sudo udhcpc -q -f -i "$wwan_interface"
         break
     else
-        echo "Device is not connected. Resetting and attempting to connect again."
+        echo "Device not connected. Resetting..."
         sudo qmicli -d "$device" --dms-set-operating-mode=reset
-        echo "Wait 22 seconds to let the module restart."
+        echo "Wait 22 seconds to let the module restart"
         sleep 22
     fi
 done
--- stop_modem.sh
+++ stop_modem.sh
@@ -4,10 +4,10 @@ device=$(ls /dev/cdc-wdm* 2>/dev/null | head -n 1)
 
 [ -z "$device" ] && { echo "No device found"; exit 1; }
 
-# Set the device to low-power mode
-sudo qmicli -d "$device" --dms-set-operating-mode="low-power"
-
-sleep 2
-
-# Stop the network connection
-sudo qmi-network "$device" stop
+# Set the device to low-power mode and stop the network connection
+if sudo qmicli -d "$device" --dms-set-operating-mode="low-power" && sudo qmi-network "$device" stop; then
+    echo "Device $device is now in low-power mode and the network is stopped."
+else
+    echo "Error: Failed to set low-power mode or stop the network."
+    exit 1
+fi
